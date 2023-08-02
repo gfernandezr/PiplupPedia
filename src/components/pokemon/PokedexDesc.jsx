3ddd03e75d33d75685146a388a9742f271d1332e
gfernandezr <gfernandezr7@gmail.com>
2023-08-02 18:29:33 -0400

    fix[fixed pokedex desc dynamic styles]

--- src/components/pokemon/PokedexDesc.jsx
+++ src/components/pokemon/PokedexDesc.jsx
@@ -4,26 +4,34 @@ function PokedexDesc({ entries, language }) {
   const es_entries = entries.filter(
     (e) => e.language.name === language && e.version.name in versions
   );
-  console.log(es_entries);
   return (
     <div className="flex flex-col gap-3 p-1">
       {es_entries.map((e, index) => (
-        <div className={`flex flex-col rounded-md bg-[${versions[e.version.name].color}]
-        md:flex-row`}>
-          <div className="flex flex-row justify-evenly p-2
-          md:w-80">
-            <img src={versions[e.version.name].icon} className="w-fit h-fit my-auto" />
-            <b className="my-auto text-gray-400">{versions[e.version.name].name_es}</b>
+        <div
+          className={`flex flex-col rounded-md bg-[${versions[e.version.name].color}] md:flex-row`}
+          key={index}
+        >
+          <div
+            className="flex flex-row justify-evenly p-2
+          md:w-80"
+          >
+            <img
+              src={versions[e.version.name].icon}
+              className="w-fit h-fit my-auto"
+            />
+            <b className="my-auto text-gray-400">
+              {versions[e.version.name].name_es}
+            </b>
           </div>
           <div className="w-full">
-          <p
-            key={index}
-            className="italic p-2 px-3 border-gray-600 h-fit
+            <p
+              key={index}
+              className="italic p-2 px-3 border-gray-600 h-fit
             bg-opacity-70 bg-white text-gray-700 text-justify rounded-b-md
             md:rounded-b-none md:rounded-r-md md:w-full md:m-0 md:h-full"
-          >
-            "{e.flavor_text}"
-          </p>
+            >
+              "{e.flavor_text}"
+            </p>
           </div>
         </div>
       ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,49 @@ export default {
     extend: {},
   },
   plugins: [],
+  mode: "jit",
+  content: [
+
+    './public/**/*.html',
+
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+
+  ],
+  safelist: [
+    'bg-[#ff0000]',
+    'bg-[#00ff00]',
+    'bg-[#0000ff]',
+    'bg-[#FFFF00]',
+    'bg-[#ff00ff]',
+    'bg-[#00ffff]',
+    'bg-[#D2C21A]',
+    'bg-[#A0A0A0]',
+    'bg-[#00B3F5]',
+    'bg-[#B80000]',
+    'bg-[#0D00B8]',
+    'bg-[#11B800]',
+    'bg-[#FF7200]',
+    'bg-[#4B9C0A]',
+    'bg-[#6077FF]',
+    'bg-[#FC72E4]',
+    'bg-[#6B6B6B]',
+    'bg-[#D19A19]',
+    'bg-[#B8B8B8]',
+    'bg-[#424242]',
+    'bg-[#F2F2F2]',
+    'bg-[#1f476c]',
+    'bg-[#d22b45]',
+    'bg-[#D4400F]',
+    'bg-[#008CB3]',
+    'bg-[#F5941D]',
+    'bg-[#b43deb]',
+    'bg-[#452600]',
+    'bg-[#230c2e]',
+    'bg-[#fff836]',
+    'bg-[#9e6237]',
+    'bg-[#00A0E9]',
+    'bg-[#e5005a]',
+  ]
+  
+  
 }
